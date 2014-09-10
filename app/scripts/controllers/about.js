be7671314598a6b3df943cf99c3460c01e5da23c
rodrigogs <rodrigo.smscom@gmail.com>
2014-09-10 08:01:47 -0300

    Initial import

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videocrawlerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the videocrawlerApp
+ */
+angular.module('videocrawlerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videocrawlerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the videocrawlerApp
+ */
+angular.module('videocrawlerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
